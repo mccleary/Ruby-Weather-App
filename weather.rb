fb694c7065153e560ddb40bed73e71d7843c33dc
Trista McCleary <trista_mccleary@yahoo.com>
2018-04-23 00:18:04 -0400

    add initial script

--- weather.rb
+++ weather.rb
@@ -1,4 +1,4 @@
-# Weather App 
+# Weather App
 
 # require Ruby Gem
 require "barometer"
@@ -37,3 +37,31 @@ weather.forecast.each do |forecast|
   # displays weather with date as month and day
   # puts forecast.starts_at.month.to_s + "/" + forecast.starts_at.day.to_s + " is going to be " + forecast.icon + " with a low of " + forecast.low.f.to_s + " and a high of " + forecast.high.f.to_s
 end
+
+
+
+
+
+# ***********  use this for current temp **************
+# current temperature per location
+# def find_location(location)
+#   barometer = Barometer.new(location)
+#   weather = barometer.measure
+#   current_temp = weather.current.temperature.f
+#
+#   if current_temp == "84"
+#     puts "It's #{current_temp} and very sunny!"
+#   elsif current_temp == "55"
+#     puts "It's #{current_temp} and very cloudy!"
+#   elsif current_temp == "50"
+#     puts "It's #{current_temp} and very cold!"
+#   elsif current_temp == "60"
+#     puts "It's #{current_temp} and very rainy!"
+#   elsif current_temp == "32"
+#     puts "It's #{current_temp} and very snowy!"
+#   else
+#     puts "It's #{current_temp}! Enjoy your day!"
+#   end
+# end
+#
+# find_location(location)
